c2f4e4a2d229a21d1eb4eb38ee78536bfc64a5bf
Jodie Cheung <jodiecheung@MacBookPro.Home>
2026-06-03 18:06:09 +0100

    update style

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
             <h1 class="school-logo-text school-logo-text--alt-color">
               <a href="#"><strong>Gardeners</strong> Companion</a>
             </h1>
-            <p><a class="site-footer__link" href="#">555.555.5555</a></p>
+        
           </div>
 
           <div class="site-footer__col-two-three-group">
--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 function gardeners_files(){
     wp_enqueue_script('main-gardeners-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Raleway:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
     wp_enqueue_style('style', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('other-styles', get_theme_file_uri('/build/index.css'));
 }
